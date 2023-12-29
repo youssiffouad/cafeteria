912f174c8b3acce6e3a5329bb93e99288ec2b2e5
unknown <youssif1999fouad@gmail.com>
2023-12-30 00:47:17 +0200

    test sad

--- src/backend/tests/addComponentLot.js
+++ src/backend/tests/addComponentLot.js
@@ -1,2 +1,4 @@
 const addcompLot = require("../businessLogic/addComponentLot");
-addcompLot(1, 200, 500, new Date(2023, 1, 1), "cash", 1);
+const Lot = require("../models/Lot");
+// addcompLot(1, 200, 500, new Date(2023, 1, 1), "cash", 3);
+Lot.viewComponentLots();
---
+++ src/backend/tests/addOrdinaryLOt.js
@@ -0,0 +1,2 @@
+const addOrdinaryLot = require("../businessLogic/addOrdinaryLot");
+addOrdinaryLot(1, 10, 50, 20, new Date(2023, 10, 5), "cash");
